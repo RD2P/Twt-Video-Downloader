256b783cf8fe5b213f3ce0c42029ddcf3c8c9130
Raphael <graphael.dr@gmail.com>
2023-04-19 19:13:56 -0400

    Adding script starter

---
+++ script_starter.py
@@ -0,0 +1,7 @@
+import os
+
+# Change directory to the location where the Python file is located
+os.chdir(r'C:\Projects\twt-vid-downloader')
+
+# Run the Python file using the command line
+os.system('working_test.py')
