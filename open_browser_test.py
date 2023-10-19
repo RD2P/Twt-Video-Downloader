ecb6c1d01a1d21b13b61db1dc2eea8711ffe2c76
Raphael <graphael.dr@gmail.com>
2023-10-19 00:01:45 -0600

    Testing webdriver after update to latest version

---
+++ open_browser_test.py
@@ -0,0 +1,8 @@
+'''
+Testing webdriver match to edge browser
+'''
+
+from selenium import webdriver
+
+# Instantiate the webdriver
+browser = webdriver.Edge("C:\Program Files (x86)\msedgedriver.exe")
