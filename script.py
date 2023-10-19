9f064121869cab0e0c64c8b7e3a6085b35128311
Raphael <graphael.dr@gmail.com>
2023-10-19 00:03:07 -0600

    Renamed main script to script.py

--- script.py
+++ script.py
@@ -1,61 +1,66 @@
 from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-from time import sleep
-import time
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.opera.options import Options
-from selenium.webdriver.support.wait import WebDriverWait
 import os
 import requests
 import tkinter as tk
 from tkinter import simpledialog
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 # Pop up asking for the link
 root = tk.Tk()
 root.withdraw()
-user_input = simpledialog.askstring("Link", "Please enter the link:")
-print(user_input)
 
-# Instantiate the webdriver with the executable location of MS Edge web driver
+# Paste link of twitter video in the pop up
+user_input = simpledialog.askstring("Link", "Please enter the link:")
+
+# Instantiate the webdriver
 browser = webdriver.Edge("C:\Program Files (x86)\msedgedriver.exe")
-# browser.maximize_window()
+
+# Use third party video downloader
 browser.get('https://twittervideodownloader.com/')
-# Find the element by ID or other locator
+
+# Grab the input box element (it has name "tweet")
 input_field = browser.find_element_by_name("tweet")
-# send link input by user into input field on browser
+
+# Send link input by user into input field on browser
 input_field.send_keys(user_input)
+
+# Find and click the "Download" button
 submit_button = browser.find_element_by_class_name("button")
 submit_button.click()
 
-# time.sleep(3)
-# Search for download button
+# Search for and click another download button
 download_button = browser.find_element_by_link_text("Download Video")
+wait = WebDriverWait(browser, 5)  # Wait up to 5 seconds
+download_button = wait.until(EC.presence_of_element_located((By.LINK_TEXT, "Download Video")))
 download_button.click()
 
 # DOWNLOAD
 # the URL of the video file to download
 url = browser.current_url
 
+# Get current dir
 current_dir = os.getcwd()
-file_path = os.path.join(current_dir + "\Downloads", "zvideo.mp4")
+
+# Specify video download location
+file_path = os.path.join(current_dir + "\Downloads", "video.mp4")
 
 # send a GET request to the URL
 response = requests.get(url)
-# check if the request was successful
+
+# Check if the request was successful
+# If successful, open the specified download location and write the video as binary data in it
 if response.status_code == 200:
-    # the request was successful, so save the video file
     with open(file_path, "wb") as f:
         f.write(response.content)
     print("Video downloaded successfully!")
 else:
-    # the request failed, so print the error message
     print(f"Error downloading video: {response.status_code} {response.reason}")
 
 browser.quit()
 
-downloads_path = os.path.expanduser(
-    r"C:\Projects\twt-vid-downloader\Downloads")
+
+# Open downloads folder to view downloaded video
+downloads_path = os.path.join(current_dir, "Downloads")
 os.startfile(downloads_path)
