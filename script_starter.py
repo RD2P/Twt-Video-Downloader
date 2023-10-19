391c0a2cb3e1cce65aa7f666153b8ec337d65d4d
Raphael <graphael.dr@gmail.com>
2023-10-19 00:51:14 -0600

    Clarified comment

--- script_starter.py
+++ script_starter.py
@@ -1,12 +1,13 @@
 '''
-I saved a shortcut of this file on the desktop so I can easily start the script.py
-If you use this script the same way, you'll have to change the path to script.py in your machine
+You could save this file on the desktop to easily start the script.py
+This way you could keep the project scripts and downloads wherever you want
+You'll just have to change the path to script.py below
 '''
 
 import os
 
 # Change directory to the location where the Python file is located
-os.chdir(r'C:\Projects\twt-vid-downloader')
+os.chdir(r'C:\Projects\Python\twt-vid-downloader')
 
 # Run the Python file using the command line
 os.system('script.py')
