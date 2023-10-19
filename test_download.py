cfdfe7e9a6c3138503f0df495a47867950f67acb
Raphael <graphael.dr@gmail.com>
2023-10-19 00:04:50 -0600

    Removed old test video url (deleted video), replaced with new one

--- test_download.py
+++ test_download.py
@@ -1,22 +1,28 @@
+'''
+Testing video download
+'''
+
+
 import requests
 import os
 
-# the URL of the video file to download
-url = "https://video.twimg.com/ext_tw_video/1634699563339481088/pu/vid/320x568/-PC_YGvHSSa6U2od.mp4?tag=12"
+# Hardcoded URL of the video file to download
+url = "https://twitter.com/BestMoment_Vids/status/1712467861942513719"
 
 current_dir = os.getcwd()
-file_path = os.path.join(current_dir, "video.mp4")
+file_path = os.path.join(current_dir, "Downloads", "video.mp4")
 
 
-# send a GET request to the URL
+# send GET request to URL
 response = requests.get(url)
 
-# check if the request was successful
+# check if request was successful
 if response.status_code == 200:
-    # the request was successful, so save the video file
+    # request successful, save video file
+    # wb is "write binary" mode, to write binary data to file
     with open(file_path, "wb") as f:
         f.write(response.content)
     print("Video downloaded successfully!")
 else:
-    # the request failed, so print the error message
+    # request failed, print the error message
     print(f"Error downloading video: {response.status_code} {response.reason}")
