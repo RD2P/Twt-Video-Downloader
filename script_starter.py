ab52b0e5446447ab352a0f6951da88b543515777
Raphael <graphael.dr@gmail.com>
2023-10-19 00:02:19 -0600

    Added comment, amended main script name

--- script_starter.py
+++ script_starter.py
@@ -1,7 +1,12 @@
+'''
+I saved a shortcut of this file on the desktop so I can easily start the script.py
+If you use this script the same way, you'll have to change the path to script.py in your machine
+'''
+
 import os
 
 # Change directory to the location where the Python file is located
 os.chdir(r'C:\Projects\twt-vid-downloader')
 
 # Run the Python file using the command line
-os.system('working_test.py')
+os.system('script.py')
